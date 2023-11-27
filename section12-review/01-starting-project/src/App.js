a0e0da55d98fa78aa007d3d9ebba376c2ff0d341
고세종 <fulfilled7418@gmail.com>
2023-11-27 19:52:00 +0900

    seciont12-review finished

--- section12-review/01-starting-project/src/App.js
+++ section12-review/01-starting-project/src/App.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [carIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <>
-      <Header></Header>
+    <CartProvider>
+      {carIsShown && <Cart onClose={hideCartHandler}></Cart>}
+      <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals></Meals>
       </main>
-    </>
+    </CartProvider>
   );
 }
 
