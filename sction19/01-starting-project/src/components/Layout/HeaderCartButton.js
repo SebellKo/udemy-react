6b4fee2a7b4c56d158fc0ecc0e4c63b8b9516d22
고세종 <fulfilled7418@gmail.com>
2023-12-23 02:22:41 +0900

    section20 finished

--- sction19/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ sction19/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,20 @@
 import { useContext, useEffect, useState } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const { items } = cartCtx;
-  console.log(items);
+
   const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
-  const btnClasses = `${classes.button} ${
-    btnIsHighlighted ? classes.bump : ''
-  }`;
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
 
   useEffect(() => {
     if (items.length === 0) {
@@ -35,7 +34,7 @@ const HeaderCartButton = (props) => {
   return (
     <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
-        <CartIcon></CartIcon>
+        <CartIcon />
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
