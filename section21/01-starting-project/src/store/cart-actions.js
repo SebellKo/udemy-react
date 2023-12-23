66d78799891e80e8d92dcd6b5e8a2a1ef2db42eb
고세종 <fulfilled7418@gmail.com>
2023-12-24 02:13:25 +0900

    section21 finished

---
+++ section21/01-starting-project/src/store/cart-actions.js
@@ -0,0 +1,72 @@
+import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch('https://example.com/cart.json');
+
+      if (!response.ok) {
+        throw new Error('error');
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Fetching cart data failed !',
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Pending...',
+        message: 'Sending data to cart...',
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch('https://example.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error('error');
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully !',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed !',
+        })
+      );
+    }
+  };
+};
