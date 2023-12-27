1d5768d60ed2df567a97ee62835c7cab54791743
고세종 <fulfilled7418@gmail.com>
2023-12-28 07:11:50 +0900

    section22-2 finished

---
+++ section22/12-adv-starting-project/frontend/src/pages/Root.js
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+
+import MainNavigation from '../components/MainNavigation';
+
+const RootLayout = () => {
+  // const navigation = useNavigation();
+
+  return (
+    <>
+      <MainNavigation></MainNavigation>
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet></Outlet>
+      </main>
+    </>
+  );
+};
+
+export default RootLayout;
